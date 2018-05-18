01a71f3b5f5d672bf2c211d3a3ff0dff2016b61a
zbloggithub <39395470+zbloggithub@users.noreply.github.com>
2018-05-18 19:58:06 +0800

    Update settings.py

--- ZBlog/settings.py
+++ ZBlog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fm!*wtc*er&8ez1v21gxp-^84(*rj6w-0v+1-i217&nmzksa=o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zblog.work']
 
